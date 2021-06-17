b33f0300b3affc2fca02e192860bdde41efa5a1d
Purusoth <sivakrubakaran@gmail.com>
2021-06-17 16:34:46 +0530

    button added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,20 @@ function App() {
     username:"Unknown",
     pwd:"Password"
   })
+  Change=()=>{
+    setItem({
+      username:"YourWebsite",
+    pwd:"12345678"
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <label>UserName:{item.username}</label>
         <label>Password:{item.pwd}</label>
+        <button onClick={this.Change}>Onchange</button>
       </header>
     </div>
   );
